6accf2129bdefe37d429e0b2ca9cabcf15d15cdb
Kiko <thekikiyo@gmail.com>
2022-04-17 10:26:12 +0200

    add docker

--- snake-front/src/env.tsx
+++ snake-front/src/env.tsx
@@ -2,8 +2,8 @@ export let snakeSocketUlr: string;
 export let apiUrl: string;
 
 if (process.env.NODE_ENV === 'production') {
-  snakeSocketUlr = 'https://socfifa.sauravmh.com/classic-mode';
-  apiUrl = 'https://apififa.sauravmh.com'
+  snakeSocketUlr = 'http://localhost:3004/classic-mode';
+  apiUrl = 'http://localhost:3003'
 } else {
   snakeSocketUlr = 'http://localhost:3004/classic-mode';
   apiUrl = 'http://localhost:3003'
